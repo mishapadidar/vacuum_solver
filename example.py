fixed scaling of current by mu0. this was a major bug. everything looks good now

example.py
@@ -23,46 +23,50 @@ surf.set('zs(0,2)', 0.1)
 surf_outer = surf.copy()
 mr = surf.minor_radius()
 surf_outer.extend_via_normal(mr*2)
-I_P = 1e5
-M = 10
-N = 10
-current = SheetCurrent(surf_outer, I_P, M, N, jit=1e-12)
-current.set('s(0,0)', 1.e5)
-current.set('s(0,1)', 1.e5)
-current.set('s(1,0)', 1.e5)
+G = 0.8
+M = 6
+N = 6
+current = SheetCurrent(surf_outer, G, M, N)
+current.set('s(0,0)', 1.0)
+current.set('s(0,1)', 1.0)
+current.set('s(1,0)', 1.0)
 
-
+# Create a 3D plot
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 X = surf.gamma()
-sc = ax.plot_surface(X[:, :, 0], X[:, :, 1], X[:, :, 2])
+sc = ax.plot_surface(X[:, :, 0], X[:, :, 1], X[:, :, 2], label='surface')
 X = surf_outer.gamma()
-sc = ax.plot_surface(X[:, :, 0], X[:, :, 1], X[:, :, 2], alpha=0.5)
+sc = ax.plot_surface(X[:, :, 0], X[:, :, 1], X[:, :, 2], alpha=0.5, label='winding surface')
 plt.tight_layout()
+plt.legend()
 plt.show()
-
 
 
 # check the squared flux
 print('initial squared flux:')
 print(current.squared_flux(surf))
 
+# fit the sheet current
 current.fit(surf)
 
 # check the squared flux
 print('final squared flux:')
 print(current.squared_flux(surf))
 
-K = current.current()
-print('final mean squared current:')
-print(np.mean(np.linalg.norm(K, axis=-1)**2))
+# compute B on axis
+X = surf.gamma() # (nphi, ntheta, 3)
+X_axis = np.mean(X, axis=1) # (nphi, 3)
+B_axis = current.B(X_axis)
+print('Average |B| on axis:')
+print(np.mean(np.linalg.norm(B_axis, axis=-1)))
 
 
 # Create a 3D plot
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 # copy the surface with more points for better visualization
-surf = surf.copy(nphi=128, ntheta=128)
+surf = surf.copy(nphi=128, ntheta=64, range='field period')
 X = surf.gamma()
 Bnormal = current.B_normal(surf)
 norm = plt.Normalize(vmin=Bnormal.min(), vmax=Bnormal.max())
@@ -71,6 +75,8 @@ sc = ax.plot_surface(X[:, :, 0], X[:, :, 1], X[:, :, 2], facecolors=colors, rstr
 mappable = plt.cm.ScalarMappable(cmap='viridis', norm=norm)
 mappable.set_array(Bnormal)
 plt.colorbar(mappable, ax=ax, shrink=0.8)
+mappable.colorbar.set_label('|B*n|')
 plt.tight_layout()
+plt.legend()
 plt.show()
 

sheet_current.py
@@ -4,32 +4,51 @@ from util import rotate_nfp
 
 
 class SheetCurrent(Optimizable):
-    def __init__(self, surface, I_P, M=8, N=8, jit=1e-13):
+    def __init__(self, surface, G, M=8, N=8, jit=1e-13):
         """
         Initialize the sheet current with the given parameters.
 
         The sheet current is defined as:
             K = n x grad(Phi)
         where 
-            Phi = tilde{Phi} + (I_P / 2 * pi) * phi
+            Phi = tilde{Phi} + G * phi
         and tilde{Phi} is doubly periodic on (theta, phi) in [0, 1] x [0, 1/nfp].
+        Boozer's G is defined as 
+            G = mu0 * I_P / (2 * pi),
+        where I_P is the sum of the currents through the hole in the torus.
 
         This class is often used to solve to find vacuum equilibrium for a given boundary.
+
+        Example usage:
+            coils = ...
+            surf = ...
+            I_P = np.abs(sum([np.abs(coil.current.get_value()) for coil in coils]))
+            mu0 =  4 * np.pi * 10**(-7)
+            G = I_P * mu0 # in Simsopt, grad(phi) absorbs the 1/2pi factor since angles are on [0, 1]
+            surf_winding = surf.to_RZFourier().copy(ntheta=2 * M + 1, nphi=2 * N + 1, range='field period')
+            surf_winding.extend_via_normal(2 * surf.minor_radius())
+            current = SheetCurrent(surf_winding, G, M, N)
+            current.fit(surf)
+            print(current.squared_flux(surf))
         
         Parameters:
-        surface (Surface): A Simsopt surface object.
-        I_P (float): Poloidal current
-        phi (float): The potential function.
-        M,N (int): Highest fourier mode number (inclusive) in the poloidal and toroidal directions.
-        jit (float): Tikhonov regularization parameter for the least squares problem.
+            surface (Surface): A Simsopt surface object.
+            G (float): Normalized poloidal current.
+            phi (float): The potential function.
+            M,N (int): Highest fourier mode number (inclusive) in the poloidal and toroidal directions.
+            jit (float): Tikhonov regularization parameter for the least squares problem.
         """
         self.surface = surface
         self.nfp = surface.nfp
         self.stellsym = surface.stellsym
-        self.I_P = I_P
+        self.G = G
         self.M = M # poloidal
         self.N = N # toroidal
         self.jit = jit # regularization parameter for the least squares problem
+
+        # constants
+        self.mu0 =  1.256637061e-6 # N / A^2
+        self.mu0_over_4pi = self.mu0 / (4 * np.pi) 
 
         self._set_names()
         
@@ -72,7 +91,7 @@ class SheetCurrent(Optimizable):
     
     def potential(self):
         """ Compute the potential at the surface quadrature points
-            Phi = tilde{Phi} + (I_P / 2 * pi) * phi
+            Phi = tilde{Phi} + G * phi
         and tilde{Phi} is doubly periodic on (theta, phi) in [0, 1] x [0, 1/nfp].
             tilde{Phi} = sum_{m=0}^{M} sum_{n=0}^{N} c(m,n) * cos(2 * pi (m * theta - nfp * n * phi )) 
                         + s(m,n) * sin(2 * pi (m * theta - nfp * n * phi ))
@@ -109,11 +128,13 @@ class SheetCurrent(Optimizable):
                     idx += 1
         
         # secular term
-        pot += (self.I_P / 2 / np.pi) * phis
+        pot += (self.G) * phis
         return pot
     
     def current(self):
-        """ Compute the sheet current at the quadrature points on S'.
+        """ Compute the sheet current at the quadrature points on S'. This function evaluates
+            K * dS' at the surface quadrature points, where K is the sheet current and S' is the
+            winding surface.
 
         Simsopt surfaces obey the ordering (phi, theta), so,
             n = dr/dphi x dr/dtheta.
@@ -123,14 +144,14 @@ class SheetCurrent(Optimizable):
             n x grad(phi) = dr/dtheta
 
         The potential is given by,
-            Phi = tilde{Phi} + (I_P / 2 * pi) * phi
+            Phi = tilde{Phi} + G * phi
         and
             tilde{Phi} = sum_{m,n} c(m,n) * cos(alpha_mn) + s(m,n) * sin(alpha_mn),
         where
             alpha_mn = 2 * pi (m * theta - nfp * n * phi ).
         The sheet current is,
-            K = n x [grad(tilde{Phi}) + (I_P / 2 * pi) * grad(phi)]
-              = (I_P / 2 * pi) * dr/dtheta + 
+            mu0 * K * dS' = n x [grad(tilde{Phi}) + G * grad(phi)]
+              = G * dr/dtheta + 
                 + sum_{m,n} [-c(m,n) * sin(alpha_mn) + s(m,n) * cos(alpha_mn)] * (n x grad(alpha_mn))
         where
             n x grad(alpha_mn) = 2 * pi (m * n x grad(theta) - nfp * n * n x grad(phi) )
@@ -177,7 +198,10 @@ class SheetCurrent(Optimizable):
                     idx += 1
             
         # secular term
-        K += (self.I_P / 2 / np.pi) * n_cross_grad_phi
+        K += self.G * n_cross_grad_phi
+
+        # scale by mu0
+        K = K / self.mu0 
 
         return K
     
@@ -209,12 +233,7 @@ class SheetCurrent(Optimizable):
 
         dphi = np.diff(self.surface.quadpoints_phi)[0]
         dtheta = np.diff(self.surface.quadpoints_theta)[0]
-        # TODO: i dont think we need |n| in dA since it is already in K?
-        # normal = self.surface.normal() # (nphi, ntheta, 3)
-        dA = dphi * dtheta #* np.linalg.norm(normal, axis=-1, keepdims=True)
-
-        mu0 =  1.256637061e-6 # N / A^2
-        mu0_over_4pi = mu0 / (4 * np.pi) 
+        dA = dphi * dtheta
 
         # compute the magnetic field using the Biot-Savart law
         B = np.zeros(np.shape(X))
@@ -223,7 +242,7 @@ class SheetCurrent(Optimizable):
             dist = np.linalg.norm(diff, axis=-1, keepdims=True) # (nphi, ntheta, 1)
             kernel = diff / (dist**3) # (nphi, ntheta, 3)
             cross = np.cross(K, kernel, axis=-1) # (nphi, ntheta, 3)
-            B[i] = mu0_over_4pi * np.sum(cross * dA, axis=(0, 1))
+            B[i] = self.mu0_over_4pi * np.sum(cross * dA, axis=(0, 1))
 
         return B
 
@@ -258,12 +277,8 @@ class SheetCurrent(Optimizable):
 
         dphi = np.diff(self.surface.quadpoints_phi)[0]
         dtheta = np.diff(self.surface.quadpoints_theta)[0]
-        # TODO: i dont think we need |n| in dA since it is already in K?
-        # normal = self.surface.normal() # (nphi, ntheta, 3)
-        dA = dphi * dtheta #* np.linalg.norm(normal, axis=-1, keepdims=True)
+        dA = dphi * dtheta
 
-        mu0 =  1.256637061e-6 # N / A^2
-        mu0_over_4pi = mu0 / (4 * np.pi) 
         eye = np.eye(3) # (3, 3)
 
         # compute the magnetic field using the Biot-Savart law
@@ -282,7 +297,7 @@ class SheetCurrent(Optimizable):
                 first_term = eye[j][None, None, :] / dist_cubed # (nphi, ntheta, 3)
                 dkernel_by_dj = first_term - diff[:,:,j][:,:,None] * second_term
                 cross = np.cross(K, dkernel_by_dj, axis=-1) # (nphi, ntheta, 3)
-                gradB[i, :, j] = mu0_over_4pi * np.sum(cross * dA, axis=(0, 1))
+                gradB[i, :, j] = self.mu0_over_4pi * np.sum(cross * dA, axis=(0, 1))
 
         return gradB
 
@@ -376,12 +391,8 @@ class SheetCurrent(Optimizable):
 
         dphi = np.diff(self.surface.quadpoints_phi)[0]
         dtheta = np.diff(self.surface.quadpoints_theta)[0]
-        # TODO: i dont think we need |n| in dA since it is already in K?
-        # normal = self.surface.normal() # (nphi, ntheta, 3)
-        self.dA = dphi * dtheta #* np.linalg.norm(normal, axis=-1, keepdims=True)
+        self.dA = dphi * dtheta 
 
-        mu0 =  1.256637061e-6 # N / A^2
-        self.mu0_over_4pi = mu0 / (4 * np.pi) 
 
     def compute_h_fourier(self, x, nhat):
         """
@@ -398,7 +409,6 @@ class SheetCurrent(Optimizable):
             np.ndarray: (ndofs,) array of the normal component of the projection of the field onto each fourier mode. The array
                 is organized in the same way as the names array.
         """
-        mu0_over_4pi = self.mu0_over_4pi # float
         dA = self.dA # (nphi, ntheta, 1)
         n_cross_grad_theta = self.n_cross_grad_theta # (nphi, ntheta, 3)
         n_cross_grad_phi = self.n_cross_grad_phi # (nphi, ntheta, 3)
@@ -417,6 +427,9 @@ class SheetCurrent(Optimizable):
         # storage
         h_array = np.zeros(self.n_dofs) # (ndofs,)
 
+        const = 1 / 4 / np.pi
+
+        # TODO: we can do this with a fourier transform!
         idx = 0
         # compute h^S
         for m in range(self.M+1):
@@ -424,7 +437,7 @@ class SheetCurrent(Optimizable):
                 fourier = np.cos(2 * np.pi * (m * thetas - self.nfp * n * phis)) # (nphi, ntheta)
                 n_cross_grad_alpha = 2 * np.pi * (m * n_cross_grad_theta  - self.nfp * n * n_cross_grad_phi) # (nphi, ntheta, 3)
                 dot = np.sum(n_cross_grad_alpha * kernel_cross_nhat, axis=-1) # (nphi, ntheta)
-                h_array[idx] = mu0_over_4pi * np.sum(fourier * dot * dA, axis=(-2, -1)) # (ndofs,)     
+                h_array[idx] = const * np.sum(fourier * dot * dA, axis=(-2, -1)) # (ndofs,)     
                 idx += 1
 
         # compute h^C
@@ -436,7 +449,7 @@ class SheetCurrent(Optimizable):
                     fourier = - np.sin(2 * np.pi * (m * thetas - self.nfp * n * phis)) # (nphi, ntheta)
                     n_cross_grad_alpha = 2 * np.pi * (m * n_cross_grad_theta  - self.nfp * n * n_cross_grad_phi) # (nphi, ntheta, 3)
                     dot = np.sum(n_cross_grad_alpha * kernel_cross_nhat, axis=-1) # (nphi, ntheta)
-                    h_array[idx] = mu0_over_4pi * np.sum(fourier * dot * dA, axis=(-2, -1)) # (ndofs,)     
+                    h_array[idx] = const * np.sum(fourier * dot * dA, axis=(-2, -1)) # (ndofs,)     
                     idx += 1
 
         return h_array
@@ -447,7 +460,7 @@ class SheetCurrent(Optimizable):
         Compute the normal field generated by the secular term
 
             h^P(r) = B^P(r) * nhat(r) = c * int [(n' x grad(phi')) x kernel(r,r')] * nhat(r) dA'
-            c = mu0_over_4pi * I_P / (2 * pi)
+            c = mu0_over_4pi * G
 
         Args:
             x (np.ndarray): (3,) array with a point in space where the secular term is evaluated.
@@ -456,7 +469,7 @@ class SheetCurrent(Optimizable):
         Returns:
             np.ndarray: The secular term for the Biot-Savart integral.
         """
-        const = self.mu0_over_4pi * self.I_P / (2 * np.pi)
+        const = self.G / 4 / np.pi
 
         # kernel
         diff = x - self.quadpoints # (nphi, ntheta, 3)

test_sheet_current.py
@@ -71,10 +71,10 @@ def test_current():
                             quadpoints_phi=np.linspace(0, 1/nfp, 128, endpoint=False),
                             quadpoints_theta=np.linspace(0, 1, 128, endpoint=False))
     surf.set('rc(0,0)', 1.0)
-    I_P = 1.0
+    G = 1.0
     M = 4
     N = 4
-    current = SheetCurrent(surf, I_P, M, N)
+    current = SheetCurrent(surf, G, M, N)
     current.set('s(0,0)', 1.23)
     current.set('s(1,0)', 2)
     current.set('s(0,1)', 0.01)
@@ -96,15 +96,15 @@ def test_B():
                             quadpoints_phi=np.linspace(0, 1/nfp, 128, endpoint=False),
                             quadpoints_theta=np.linspace(0, 1, 128, endpoint=False))
     surf.set('rc(0,0)', 1.0)
-    I_P = 1e5
+    G = 1e0
     M = 3
     N = 3
-    current = SheetCurrent(surf, I_P, M, N)
-    current.set('s(0,0)', 1.23e5)
-    current.set('s(1,0)', 2e5)
-    current.set('s(0,1)', 2e5)
-    current.set('c(0,1)', 2e5)
-    current.set('s(0,3)', 0.01 * I_P)
+    current = SheetCurrent(surf, G, M, N)
+    current.set('s(0,0)', 1.23)
+    current.set('s(1,0)', 2)
+    current.set('s(0,1)', 2)
+    current.set('c(0,1)', 2)
+    current.set('s(0,3)', 0.01 * G)
 
     def magfield(x):
         """ compute the magnetic field at x """
@@ -113,10 +113,11 @@ def test_B():
     x0 = np.array([0.5, 0.03, 0.07])
 
     # check gradB accuracy with finite difference
-    gradB_fd = finite_difference(magfield, x0, eps=1e-5)
+    gradB_fd = finite_difference(magfield, x0, eps=1e-6)
 
     # test divergence is zero
     err = np.trace(gradB_fd)
+    print('divergence err', err)
     assert np.abs(err) < 1e-8, "Divergence of B is not zero: error = {}".format(err)
 
     # test curl is zero
@@ -125,6 +126,7 @@ def test_B():
     curlB_fd[1] = gradB_fd[0, 2] - gradB_fd[2, 0]
     curlB_fd[2] = gradB_fd[1, 0] - gradB_fd[0, 1]
     err = np.max(np.abs(curlB_fd))
+    print('curlB_fd err', err)
     assert err < 1e-8, "Curl of B is not zero: max error = {}".format(err)
 
     """ Now test with multiple nfp"""
@@ -133,14 +135,14 @@ def test_B():
                             quadpoints_phi=np.linspace(0, 1/nfp, 128, endpoint=False),
                             quadpoints_theta=np.linspace(0, 1, 128, endpoint=False))
     surf.set('rc(0,0)', 1.0)
-    I_P = 1e5
+    G = 1e0
     M = 3
     N = 3
-    current = SheetCurrent(surf, I_P, M, N)
-    current.set('s(0,0)', 1.23e5)
-    current.set('s(1,0)', 2e5)
-    current.set('s(0,1)', 2e5)
-    current.set('s(0,3)', 0.01 * I_P)
+    current = SheetCurrent(surf, G, M, N)
+    current.set('s(0,0)', 1.23)
+    current.set('s(1,0)', 2)
+    current.set('s(0,1)', 2)
+    current.set('s(0,3)', 0.01 * G)
 
     def magfield(x):
         """ compute the magnetic field at x """
@@ -153,6 +155,7 @@ def test_B():
 
     # test divergence is zero
     err = np.trace(gradB_fd)
+    print('divergence err', err)
     assert np.abs(err) < 1e-8, "Divergence of B is not zero: error = {}".format(err)
 
     # test curl is zero
@@ -161,6 +164,7 @@ def test_B():
     curlB_fd[1] = gradB_fd[0, 2] - gradB_fd[2, 0]
     curlB_fd[2] = gradB_fd[1, 0] - gradB_fd[0, 1]
     err = np.max(np.abs(curlB_fd))
+    print('curlB_fd err', err)
     assert err < 1e-8, "Curl of B is not zero: max error = {}".format(err)
 
 
@@ -172,15 +176,15 @@ def test_gradB():
                             quadpoints_phi=np.linspace(0, 1/nfp, 128, endpoint=False),
                             quadpoints_theta=np.linspace(0, 1, 128, endpoint=False))
     surf.set('rc(0,0)', 1.0)
-    I_P = 1e5
+    G = 1e0
     M = 3
     N = 3
-    current = SheetCurrent(surf, I_P, M, N)
-    current.set('s(0,0)', 1.23e5)
-    current.set('s(1,0)', 2e5)
-    current.set('s(0,1)', 2e5)
-    current.set('c(0,1)', 2e5)
-    current.set('s(0,3)', 0.01 * I_P)
+    current = SheetCurrent(surf, G, M, N)
+    current.set('s(0,0)', 1.23)
+    current.set('s(1,0)', 2)
+    current.set('s(0,1)', 2)
+    current.set('c(0,1)', 2)
+    current.set('s(0,3)', 0.01 * G)
 
     def magfield(x):
         """ compute the magnetic field at x """
@@ -196,6 +200,7 @@ def test_gradB():
 
     # check the gradient of B is accurate
     err = np.max(np.abs(gradB - gradB_fd))
+    print('gradB_fd err', err)
     assert err < 1e-8, "Gradient of B is not accurate: max error = {}".format(err)
 
     """ Now test with multiple nfp"""
@@ -205,14 +210,14 @@ def test_gradB():
                             quadpoints_phi=np.linspace(0, 1/nfp, 128, endpoint=False),
                             quadpoints_theta=np.linspace(0, 1, 128, endpoint=False))
     surf.set('rc(0,0)', 1.0)
-    I_P = 1e5
+    G = 1e0
     M = 3
     N = 3
-    current = SheetCurrent(surf, I_P, M, N)
-    current.set('s(0,0)', 1.23e5)
-    current.set('s(1,0)', 2e5)
-    current.set('s(0,1)', 2e5)
-    current.set('s(0,3)', 0.01 * I_P)
+    current = SheetCurrent(surf, G, M, N)
+    current.set('s(0,0)', 1.23)
+    current.set('s(1,0)', 2)
+    current.set('s(0,1)', 2)
+    current.set('s(0,3)', 0.01 * G)
 
     def magfield(x):
         """ compute the magnetic field at x """
@@ -228,6 +233,7 @@ def test_gradB():
 
     # check the gradient of B is accurate
     err = np.max(np.abs(gradB - gradB_fd))
+    print('gradB_fd err', err)
     assert err < 1e-8, "Gradient of B is not accurate: max error = {}".format(err)
 
 
@@ -241,10 +247,10 @@ def test_fit():
                             quadpoints_phi=np.linspace(0, 1/nfp, 128, endpoint=False),
                             quadpoints_theta=np.linspace(0, 1, 128, endpoint=False))
     surf.set('rc(0,0)', 1.0)
-    I_P = 1.0
+    G = 1.0
     M = 2
     N = 2
-    current = SheetCurrent(surf, I_P, M, N, jit = 0.0)
+    current = SheetCurrent(surf, G, M, N, jit = 0.0)
 
     """ Test a linear system with a unique solution"""
 
@@ -269,7 +275,7 @@ def test_fit():
     """ Test a least squares problem"""
 
     jit = 1e-6
-    current = SheetCurrent(surf, I_P, M, N, jit = jit)
+    current = SheetCurrent(surf, G, M, N, jit = jit)
 
     # Mock the build_linear_system method
     np.random.seed(0)
@@ -295,20 +301,24 @@ def test_secular_field():
     surf = SurfaceRZFourier(nfp, stellsym=True, mpol=2, ntor=2, 
                             quadpoints_phi=np.linspace(0, 1/nfp, 17, endpoint=False),
                             quadpoints_theta=np.linspace(0, 1, 17, endpoint=False))
+    
+    # we need toroidal shaping to test secular term
+    surf.set('rc(0,0)', 1.0)
+    surf.set('rc(0,1)', 0.1)
+    surf.set('zs(0,2)', 0.1)
 
     # initialize the SheetCurrent
     surf_outer = surf.copy()
     surf_outer.extend_via_normal(surf.get('rc(0,0)') / 8)
-    I_P = 1.2e5
+    G = 1.2
     M = 2
     N = 2
-    current = SheetCurrent(surf_outer, I_P, M, N, jit = 0.0)
+    current = SheetCurrent(surf_outer, G, M, N, jit = 0.0)
 
     # biot savart returns secular term when fourier coeffs are zero
     X = surf.gamma().reshape(-1, 3) # (nphi * ntheta, 3)
     nhat = surf.unitnormal().reshape(-1, 3) # (nphi * ntheta, 3)
-    B = current.B(X)
-    h_secular = np.sum(B * nhat, axis=-1)  # (nphi * ntheta,)
+    h_secular = current.B_normal(surf).reshape(-1)  # (nphi * ntheta,)
 
     # compute secular term
     current.biot_savart_precomputation()
@@ -316,31 +326,35 @@ def test_secular_field():
         # alternative computation of secular term
         h_secular_alt = current.compute_h_secular(x_target, nhat[ii])
         err = h_secular[ii] - h_secular_alt
-        assert err < 1e-12, "Secular term is not correct: error = {}".format(err)
+        # print("Secular term error at point {}: {}".format(ii, err))
+        assert err < 1e-14, "Secular term is not correct: error = {}".format(err)
 
 def test_fourier_field():
     """ Test the fourier magnetic field in the SheetCurrent class. """
     # Create a surface
     nfp = 3
-    surf = SurfaceRZFourier(nfp, stellsym=False, mpol=1, ntor=1, 
+    surf = SurfaceRZFourier(nfp, stellsym=False, mpol=2, ntor=2, 
                             quadpoints_phi=np.linspace(0, 1/nfp, 7, endpoint=False),
                             quadpoints_theta=np.linspace(0, 1, 7, endpoint=False))
+    surf.set('rc(0,0)', 1.0)
+    surf.set('rc(0,1)', 0.1)
+    surf.set('zs(0,2)', 0.1)
 
     # initialize the SheetCurrent
     surf_outer = surf.copy()
     surf_outer.extend_via_normal(surf.get('rc(0,0)') / 8)
     
-    # set I_P to zero to remove secular term
-    I_P = 0.0
+    # set G to zero to remove secular term
+    G = 0.0
 
-    current = SheetCurrent(surf_outer, I_P, 2, 2, jit = 0.0)
+    current = SheetCurrent(surf_outer, G, 2, 2, jit = 0.0)
 
     # only turn on one fourier term
-    current.set('s(1,2)', 1.1e5)
-    current.set('s(1,0)', 1.2e5)
-    current.set('s(0,1)', 1.3e5)
-    current.set('s(1,1)', 0.932e5)
-    current.set('c(1,1)', 0.932e5)
+    current.set('s(1,2)', 1.1)
+    current.set('s(1,0)', 1.2)
+    current.set('s(0,1)', 1.3)
+    current.set('s(1,1)', 0.932)
+    current.set('c(1,1)', 0.932)
 
     # compute B*n
     B_dot_n = current.B_normal(surf).reshape(-1)  # (nphi * ntheta,)
@@ -353,7 +367,7 @@ def test_fourier_field():
         h_fourier_alt = current.compute_h_fourier(x_target, nhat[ii])
         B_dot_n_alt = np.sum(h_fourier_alt * current.local_full_x) # since we only have one fourier term
         err = np.abs(B_dot_n[ii] - B_dot_n_alt)
-        assert err < 1e-12, "Fourier term is not correct: error = {}".format(err)
+        assert err < 1e-14, "Fourier term is not correct: error = {}".format(err)
 
 
 def test_linear_system():
@@ -371,12 +385,12 @@ def test_linear_system():
     # initialize the SheetCurrent
     surf_outer = surf.copy()
     surf_outer.extend_via_normal(surf.get('rc(0,0)') / 8)
-    I_P = 1.2e5
+    G = 1.2
     M = 2
     N = 2
-    current = SheetCurrent(surf_outer, I_P, M, N, jit = 0.0)
-    current.set('s(0,0)', 1.0e5)  # set the current at the M=0, N=0 mode
-    current.set('s(0,1)', 1.1e5)  # set the current at the M=1, N=0 mode to zero
+    current = SheetCurrent(surf_outer, G, M, N, jit = 0.0)
+    current.set('s(0,0)', 1.0)  # set the current at the M=0, N=0 mode
+    current.set('s(0,1)', 1.1)  # set the current at the M=1, N=0 mode to zero
 
     H, y = current.build_linear_system(surf)
 